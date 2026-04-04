189453230ed67435dcd4b327ca4eee5630f52126
muhammad-abdullah11 <abdullahworld111@gmail.com>
2026-04-04 13:36:25 +0500

    chore: setup Clerk authentication provider and application routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./components/pages/Home";
 import Chat from "./components/pages/Chat";
+import Header from "./components/Header";
 
 export default function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/chat" element={<Chat />} />
